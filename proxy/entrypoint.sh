cefe5c46b8b78862f515d8aea93367f14a6cc0cf
BecodReyes <i@muir.fun>
2022-06-11 20:22:31 +0000

    update

--- proxy/entrypoint.sh
+++ proxy/entrypoint.sh
@@ -5,7 +5,7 @@ for i in `cat /.env`;do
 	sourceserver=`echo $i|awk -F',' '{print $2}'`
 	xrayport=`echo $i|awk -F',' '{print $3}'`
 	xrayconf=`echo $i|awk -F',' '{print $4}'`
-	nohup /usr/bin/xray run ${xrayconf} > /xray_${xrayport}_log.out 2>&1 &
+	nohup /usr/bin/xray run -c ${xrayconf} > /xray_${xrayport}_log.out 2>&1 &
 	nohup /usr/bin/socat -d TCP4-LISTEN:${socatport},reuseaddr,fork SOCKS5:127.0.0.1:${sourceserver},socks5port=${xrayport} > /socket_${socatport}_log.out 2>&1 &
 	echo "[Info] "${xrayconf}","${xrayport}","${socatport}"创建成功"
 done
