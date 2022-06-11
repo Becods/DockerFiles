5453801d102f406d042cefe0ce1de7589f87562d
BecodReyes <i@muir.fun>
2022-06-11 20:44:11 +0000

    update

--- proxy/README.md
+++ proxy/README.md
@@ -2,5 +2,5 @@
 
 ```
 SOCAT绑定端口,SOCAT代理服务器,XRAY绑定端口,XRAY配置位置
-18888,api.bilibili.com:443,10808,/xray.conf
+18888,api.bilibili.com:443,10809,/xray.json
 ```
--- proxy/entrypoint.sh
+++ proxy/entrypoint.sh
@@ -6,7 +6,7 @@ for i in `cat /.env`;do
 	xrayport=`echo $i|awk -F',' '{print $3}'`
 	xrayconf=`echo $i|awk -F',' '{print $4}'`
 	nohup /usr/bin/xray run -c ${xrayconf} > /xray_${xrayport}_log.out 2>&1 &
-	nohup /usr/bin/socat -d TCP4-LISTEN:${socatport},reuseaddr,fork SOCKS5:127.0.0.1:${sourceserver},socks5port=${xrayport} > /socket_${socatport}_log.out 2>&1 &
+	nohup /usr/bin/socat -d TCP4-LISTEN:${socatport},reuseaddr,fork PROXY:127.0.0.1:${sourceserver},proxyport=${xrayport} > /socket_${socatport}_log.out 2>&1 &
 	echo "[Info] "${xrayconf}","${xrayport}","${socatport}"创建成功"
 done
 
